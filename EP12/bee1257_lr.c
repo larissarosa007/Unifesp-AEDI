86e75169532d78150f23cf070ab85047ea180d05
larissarosa007 <130061409+larissarosa007@users.noreply.github.com>
2023-06-12 14:40:13 -0300

    bee1257_lr.c

--- EP12/bee1257_lr.c
+++ EP12/bee1257_lr.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
 //exercicio beecrowd1257 Array Hash
 
 int main() {
 
-    int N, hash_tmp, hash_total;
-    int i;
-    char st;
-
+    int N, L, hash_tmp[50];
+    int alf, pos, ele, tam, hash[50];
+    int i, j, k=0, m;
+    char st[50];
 
     scanf("%d", &N);
 
     for(i=0;i<N;i++){
+        for(j=0;j<L;j++){
+            scanf("%s", &st);
+            tam=strlen(st);
 
-    //Valor=Pos. alfabeto)+(Elemento de entrada)+(Pos. elemento)
-        scanf
+            for(m=0;m<tam;m++){
+                    alf=st[m]-65;
+                    ele=L;
+                    pos=m;
+                    hash[m]=((alf+ele)+pos);
+                    hash_tmp[N]=hash_tmp[N]+hash[m];
+            }
+        }
+
+    printf("%d", hash_tmp[N]);
     }
-
 
     return 0;
 }
