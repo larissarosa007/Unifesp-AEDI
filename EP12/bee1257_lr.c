8b24b290692e78a9ecca7b8ea3bc5a996fb7c65b
larissarosa007 <130061409+larissarosa007@users.noreply.github.com>
2023-06-12 13:58:52 -0300

    Add files via upload

---
+++ EP12/bee1257_lr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//exercicio beecrowd1257 Array Hash
+
+int main() {
+
+    int N, hash_tmp, hash_total;
+    int i;
+    char st;
+
+
+    scanf("%d", &N);
+
+    for(i=0;i<N;i++){
+
+    //Valor=Pos. alfabeto)+(Elemento de entrada)+(Pos. elemento)
+        scanf
+    }
+
+
+    return 0;
+}
